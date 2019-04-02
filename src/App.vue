59a2988b72a015645db3a7204449203072e986b5
李小亮 <1396595541@qq.com>
2019-04-02 23:34:29 +0800

    数字++demo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- 初始值 -->
+    {{$store.state.count}}
+    <!-- 调用mutation中的方法 -->
+    <button @click="$store.commit('setcount')">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+Vue.config.productionTip = false;
+// vuex创建store
+const store = new Vuex.Store({
+  // 状态（数据）
+  state: {
+    count: 0
+  },
+  // 改变状态
+  mutations: {
+    setcount(state) {
+      state.count++;
+    }
+  }
+});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+});
